c8cda8018396c023f76f597f356e1b6226c9e74b
Disty0 <disty@disty.xyz>
2025-04-24 00:25:27 +0300

    Fix PYTHONPATH on Linux

--- installer.py
+++ installer.py
@@ -974,7 +974,8 @@ def run_extension_installer(folder):
         env = os.environ.copy()
         env['PYTHONPATH'] = os.path.abspath(".")
         if os.environ.get('PYTHONPATH', None) is not None:
-            env['PYTHONPATH'] += f";{os.environ.get('PYTHONPATH', None)}"
+            seperator = ';' if sys.platform == 'win32' else ':'
+            env['PYTHONPATH'] += seperator + os.environ.get('PYTHONPATH', None)
         result = subprocess.run(f'"{sys.executable}" "{path_installer}"', shell=True, env=env, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder)
         txt = result.stdout.decode(encoding="utf8", errors="ignore")
         debug(f'Extension installer: file="{path_installer}" {txt}')
