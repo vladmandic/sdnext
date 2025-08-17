85d494ee840c6efc7fc51f698862c422df6fb1cf
Disty0 <disty@disty.xyz>
2025-08-17 05:19:44 +0300

    revert .t_().contiguous().t_()

--- modules/sdnq/__init__.py
+++ modules/sdnq/__init__.py
@@ -164,7 +164,7 @@ def sdnq_quantize_layer(layer, weights_dtype="int8", torch_dtype=None, group_siz
             if not dtype_dict[weights_dtype]["is_integer"]:
                 stride = layer.weight.stride()
                 if stride[0] > stride[1] and stride[1] == 1:
-                    layer.weight.data = layer.weight.t_().contiguous().t_()
+                    layer.weight.data = layer.weight.t().contiguous().t()
                 if not use_tensorwise_fp8_matmul:
                     scale = scale.to(torch.float32)
 
