a46f32b3542e7fe4ae2be90186b1466d9c3e2bba
Disty0 <disty@disty.xyz>
2025-11-28 01:10:05 +0300

    pull sdnq version from .common

--- modules/sdnq/__init__.py
+++ modules/sdnq/__init__.py
@@ -1,7 +1,8 @@
 from .quantizer import QuantizationMethod, SDNQConfig, SDNQQuantizer, sdnq_post_load_quant, apply_sdnq_to_module, sdnq_quantize_layer
 from .loader import save_sdnq_model, load_sdnq_model
+from .common import sdnq_version
 
-__version__ = "0.1.0"
+__version__ = sdnq_version
 
 __all__ = [
     "QuantizationMethod",
