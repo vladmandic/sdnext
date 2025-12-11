4816455a99a54409ab9ae91fb67793fad824c94c
awsr <43862868+awsr@users.noreply.github.com>
2025-12-11 08:20:47 -0800

    Don't try to read txt file as json

--- modules/lora/network.py
+++ modules/lora/network.py
@@ -116,7 +116,8 @@ class NetworkOnDisk:
         if self.filename is not None:
             fn = os.path.splitext(self.filename)[0] + '.txt'
             if os.path.exists(fn):
-                return shared.readfile(fn, silent=True)
+                with open(fn, "r") as file:
+                    return file.read()
         return None
 
     def get_alias(self):
