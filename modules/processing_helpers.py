a7b69cd805c9a1829618f9bb1eba078070540629
vladmandic <mandic00@live.com>
2026-01-20 18:33:39 +0100

    fix get_fixed_seed
    Signed-off-by: vladmandic <mandic00@live.com>

--- modules/processing_helpers.py
+++ modules/processing_helpers.py
@@ -227,7 +227,7 @@ def decode_first_stage(model, x):
 
 
 def get_fixed_seed(seed):
-    if seed is None or seed == '' or seed == -1:
+    if (seed is None) or (seed == '') or (seed == -1):
         random.seed()
         seed = int(random.randrange(4294967294))
     return seed
