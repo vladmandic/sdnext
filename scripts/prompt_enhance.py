6821d92a9211e9c1fc91859fe256c3d09936816a
vladmandic <mandic00@live.com>
2026-03-13 13:05:34 +0100

    use aux for prompt enhance
    Signed-off-by: vladmandic <mandic00@live.com>

--- scripts/prompt_enhance.py
+++ scripts/prompt_enhance.py
@@ -11,7 +11,7 @@ import gradio as gr
 from PIL import Image
 from modules import scripts_manager, shared, devices, errors, processing, sd_models, sd_modules, timer, ui_symbols
 from modules import ui_control_helpers
-from modules.sd_offload import register_aux, deregister_aux, move_aux_to_gpu, offload_aux
+from modules.sd_offload_aux import register_aux, deregister_aux, move_aux_to_gpu, offload_aux
 from modules.logger import log
 
 
