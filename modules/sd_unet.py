375591ecdddb3b51552fc1b97570c7ba549f1057
awsr <43862868+awsr@users.noreply.github.com>
2026-03-25 13:39:13 -0700

    Revert non-critical changes
    The following errors are re-introduced and will need follow-up:
    - models.py, line 92
    - sd_unet.py, line 41
    - sd_models.py, line 1440

--- modules/sd_unet.py
+++ modules/sd_unet.py
@@ -38,8 +38,8 @@ def load_unet_sdxl_nunchaku(repo_id):
 def load_unet(model, repo_id: str | None = None):
     global loaded_unet  # pylint: disable=global-statement
 
-    if ("StableDiffusionXLPipeline" in model.__class__.__name__) and repo_id is not None and (("stable-diffusion-xl-base" in repo_id) or ("sdxl-turbo" in repo_id)):
-        if model_quant.check_nunchaku("Model"):
+    if ("StableDiffusionXLPipeline" in model.__class__.__name__) and (('stable-diffusion-xl-base' in repo_id) or ('sdxl-turbo' in repo_id)):
+        if model_quant.check_nunchaku('Model'):
             unet = load_unet_sdxl_nunchaku(repo_id)
             if unet is not None:
                 model.unet = unet
