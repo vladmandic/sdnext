202f12ea6c0951e87a57c4769e8aa328897be52d
Disty0 <disty@disty.xyz>
2026-04-15 23:12:46 +0300

    SDNQ disable fp8 mm with pre-quants on unsupported gpus

--- modules/sdnq/common.py
+++ modules/sdnq/common.py
@@ -6,7 +6,7 @@ import torch
 
 from modules import shared, devices
 
-sdnq_version = "0.1.7"
+sdnq_version = "0.1.8"
 
 dtype_dict = {
     ### Integers
@@ -334,6 +334,17 @@ use_torch_compile = shared.opts.sdnq_dequantize_compile # this setting requires 
 def check_torch_compile(): # dynamo can be disabled after startup
     return use_torch_compile and not torch._dynamo.config.disable # pylint: disable=protected-access
 
+
+if os.environ.get("SDNQ_ALLOW_FP8_MM", None) is None:
+    if devices.backend == "cuda":
+        is_fp8_mm_supported = bool(torch.cuda.get_device_capability(devices.device) >= (8,9))
+    elif devices.backend == "rocm":
+        gfx_version = devices.get_hip_agent().gfx_version
+        is_fp8_mm_supported = bool(gfx_version >= 0x1200 or (gfx_version >= 0x940 and gfx_version < 0x1000))
+    else:
+        is_fp8_mm_supported = False
+else:
+    is_fp8_mm_supported = os.environ.get("SDNQ_ALLOW_FP8_MM", "0").lower() not in {"0", "false", "no"}
 
 if os.environ.get("SDNQ_USE_TENSORWISE_FP8_MM", None) is None:
     # row-wise FP8 only exist on H100 hardware, sdnq will use software row-wise with tensorwise hardware with this setting
