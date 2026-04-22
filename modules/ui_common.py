b18de2e9b563651472ce21a1dee160e47ca3857d
awsr <43862868+awsr@users.noreply.github.com>
2026-04-22 15:19:46 -0700

    Fix display typo

--- modules/ui_common.py
+++ modules/ui_common.py
@@ -509,7 +509,7 @@ def update_token_counter(text: str):
         token_counts = [len(group) - int(has_bos_token) - int(has_eos_token) for group in ids]
         if len(token_counts) > 1:
             visible = True
-            count_formatted = f"{token_counts}/{sum(token_counts)}"
+            count_formatted = f"{token_counts} {sum(token_counts)}"
         elif len(token_counts) == 1 and token_counts[0] > 0:
             visible = True
             count_formatted = str(token_counts[0])
