c855a5146af40ff2db1932c853c8a916508154d0
Vladimir Mandic <mandic00@live.com>
2026-05-01 16:34:38 +0200

    cleanup
    Signed-off-by: Vladimir Mandic <mandic00@live.com>

--- modules/masking.py
+++ modules/masking.py
@@ -406,7 +406,6 @@ def run_mask(input_image: Image.Image, input_mask: Image.Image | None = None, re
     try:
         size = min(input_image.width, input_image.height)
     except Exception as e:
-        log.error(f'Mask input image error: {e}')
         return input_mask
     if invert is not None:
         opts.invert = invert
