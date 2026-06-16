d6b475910870fe3cb190aad26ffddec3404080e8
Pablo Hellmann <phtijger@gmail.com>
2026-06-16 03:38:16 +0200

    Revert "Added wd-vit-tagger-v3 large variant"
    This reverts commit 6b568014bd144e924b0a5af5f5327e9a5abb13c7.

--- modules/caption/waifudiffusion.py
+++ modules/caption/waifudiffusion.py
@@ -22,7 +22,6 @@ load_lock = threading.Lock()
 WAIFUDIFFUSION_MODELS = {
     # v3 models (latest, recommended)
     "wd-eva02-large-tagger-v3": "SmilingWolf/wd-eva02-large-tagger-v3",
-    "wd-vit-large-tagger-v3": "SmilingWolf/wd-vit-large-tagger-v3",
     "wd-vit-tagger-v3": "SmilingWolf/wd-vit-tagger-v3",
     "wd-convnext-tagger-v3": "SmilingWolf/wd-convnext-tagger-v3",
     "wd-swinv2-tagger-v3": "SmilingWolf/wd-swinv2-tagger-v3",
