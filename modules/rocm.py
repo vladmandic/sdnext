daa27a997796c59b4d63f6e5ffecc0fb34427835
0xDELUXA <djernovevo@gmail.com>
2026-06-23 00:06:03 +0300

    fix: detect hipBLASLt Tensile library in nested per-arch subfolder

--- modules/rocm.py
+++ modules/rocm.py
@@ -113,7 +113,8 @@ class Agent:
         else:
             self.arch = MicroArchitecture.GCN
         self.is_apu = (self.gfx_version & 0xFFF0 == 0x1150) or self.gfx_version in (0x801, 0x902, 0x90c, 0x1013, 0x1033, 0x1035, 0x1036, 0x1103,)
-        self.blaslt_supported = False if blaslt_tensile_libpath is None else os.path.exists(os.path.join(blaslt_tensile_libpath, f"TensileLibrary_lazy_{self.name}.dat"))
+        blaslt_filename = f"TensileLibrary_lazy_{self.name}.dat"
+        self.blaslt_supported = blaslt_tensile_libpath is not None and (os.path.exists(os.path.join(blaslt_tensile_libpath, blaslt_filename)) or os.path.exists(os.path.join(blaslt_tensile_libpath, self.name, blaslt_filename)))
 
     def __str__(self) -> str:
         return self.name
