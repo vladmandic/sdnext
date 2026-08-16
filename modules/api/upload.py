6b499b7d17ffa8ea5bb7729c412cd0e0b46e9934
Vladimir Mandic <mandic00@live.com>
2026-08-16 19:00:55 +0200

    cleanup
    Signed-off-by: Vladimir Mandic <mandic00@live.com>

--- modules/api/upload.py
+++ modules/api/upload.py
@@ -1,7 +1,6 @@
 import os
 import tempfile
 from pathlib import Path
-from unicodedata import name
 from pydantic import BaseModel
 from fastapi import Request, Header, UploadFile, Form
 from fastapi.exceptions import HTTPException
