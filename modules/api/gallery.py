c5e29e3d7b3b5b3ec700d4d540622650c915523e
Vladimir Mandic <mandic00@live.com>
2026-08-23 11:28:50 +0200

    api schema updates
    Signed-off-by: Vladimir Mandic <mandic00@live.com>

--- modules/api/gallery.py
+++ modules/api/gallery.py
@@ -36,6 +36,10 @@ OPTS_FOLDERS = [
 class ReqFiles(BaseModel):
     folder: str = Field(title="Folder")
 
+class ItemFolder(BaseModel):
+    path: str = Field(title="Path", description="Folder path")
+    label: str = Field(title="Label", description="Folder display label")
+
 ### ws connection manager
 
 class ConnectionManager:
@@ -245,7 +249,7 @@ def register_api(api): # register api
             log.error(f'Gallery: {folder} {e}')
             return []
 
-    api.add_api_route("/sdapi/v1/browser/folders", get_folders, methods=["GET"], response_model=list[str])
+    api.add_api_route("/sdapi/v1/browser/folders", get_folders, methods=["GET"], response_model=list[ItemFolder])
     api.add_api_route("/sdapi/v1/browser/thumb", get_thumb, methods=["GET"], response_model=dict)
     api.add_api_route("/sdapi/v1/browser/files", ht_files, methods=["GET"], response_model=list)
 
